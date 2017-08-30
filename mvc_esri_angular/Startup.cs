f67472d25b020b3e92b0f2ddf92caed808cf590c
Jordan S Parfitt <jparfitt@usgs.gov>
2017-08-30 12:00:00 -0600

    fresh project

---
+++ mvc_esri_angular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_esri_angular.Startup))]
+namespace mvc_esri_angular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
